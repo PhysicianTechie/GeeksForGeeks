d75644e3554a15090ce257a5a98b86cdb5b30739
PhysicianTechie <68545264+PhysicianTechie@users.noreply.github.com>
2024-02-17 17:49:18 +0800

    Add files via upload

---
+++ Python_Learning.py
@@ -0,0 +1,8 @@
+# Geeks for Geeks for PYTHON LEARNING AND PRACTICE
+
+# taking multiple inputs from python
+x , y,z = input("enter three values: ").split()
+print("total no of students: ", x)
+print("total no of boys: ", y)
+print("total no.of girls :", z)
+
